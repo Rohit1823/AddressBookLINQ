47c7bad2b3345a2619768d82dea09892b40a871f
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-11 14:56:02 +0530

    [Rohit]ADD: Insert new contacts to address book data table

--- AddressBookLinq/AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -18,6 +18,12 @@ namespace AddressBookLinq
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("Rohit", "Machale", "South palace", "Pune", "Maharashtra", "535501", "8975596720", "rohit@gmail.com");
+            table.Rows.Add("Shivam", "Satpute", "KP", "Nagpur", "Maharashtra", "546489", "8570456737", "shivam@gmail.com");
+            table.Rows.Add("Jyosmita", "Das", "New colony", "Surat", "Gujrat", "546362", "9878678593", "jyodas@gmail.com");
+            table.Rows.Add("Charan", "Ketha", "WhiteField", "Banglore", "Karnataka", "125445", "7206326427", "charan@gmail.com");
+
         }
     }
 }
