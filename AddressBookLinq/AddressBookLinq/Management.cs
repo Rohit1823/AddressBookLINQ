2a4740eb581350194d101e49d0cb64bc0471e7ea
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-11 14:28:26 +0530

    [Rohit]ADD: Create address book data table

---
+++ AddressBookLinq/AddressBookLinq/Management.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLinq
+{
+    class Management
+    {
+        DataTable table = new DataTable("AddressBook");
+    }
+}
